fd67b560c184f4a36bafa94a3f5e48f0dc2c2adc
mahmutsbl21 <92419976+mahmutsbl21@users.noreply.github.com>
2021-10-19 13:09:32 +0300

    Update ilkdeneme.py

--- ilkdeneme.py
+++ ilkdeneme.py
@@ -1,27 +1,20 @@
-matematik =
-turkce =
-kimya =
-fizik =
-biyoloji =
-tarih =
+matematik =51
+turkce =51
+kimya =51
+fizik =51
+biyoloji =51
+tarih =51
 toplam=(float(matematik)+float(turkce)+float(kimya)+float(fizik)+float(biyoloji)+float(tarih)*25)/30
 print("Toplam :{0} ".format(toplam))
 if(toplam<50):
-  print("Sınıfta kaldınız")
+	print("Sınıfta kaldınız");
 else:
-  print("Yaşasın sınıfı geçtin")
-
-
-
-if(toplam<75):
-  print("Yaşasın sınıfı geçtin")
-else:
-  print("Teşşekür Belgesi KAZANDIN")
-
-
-
-
-if(toplam<85):
-  print("Teşşekür Belgesi KAZANDIN")
-else:
-  print("Taktir Belgesi KAZANDIN")
+	print("Yaşasın sınıfı geçtin");
+	if(toplam<75):
+		print("Yaşasın sınıfı geçtin");
+	else:
+		print("Teşşekür Belgesi KAZANDIN");
+		if(toplam<85):
+			print("Teşşekür Belgesi KAZANDIN");
+		else:
+			print("Taktir Belgesi KAZANDIN");
